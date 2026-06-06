d174d3e886c8a84fe80ffea96b9467bf948e362e
Eduardo Reinoso <ereinosov@uteq.edu.ec>
2026-06-05 19:44:54 -0500

    Update SupabaseManager.kt no api key

--- app/src/main/java/com/uteq/software/app4/services/SupabaseManager.kt
+++ app/src/main/java/com/uteq/software/app4/services/SupabaseManager.kt
@@ -5,8 +5,8 @@ import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseManager {
     val client = createSupabaseClient(
-        supabaseUrl = "https://hnwamldwpxezbjtcmrup.supabase.co",
-        supabaseKey = "sb_secret_byEO8ZhzqgfwACTPG4h2Xw_B-4crbX4"
+        supabaseUrl = "",
+        supabaseKey = ""
     ) {
         install(Postgrest)
     }
